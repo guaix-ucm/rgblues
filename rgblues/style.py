9f4637e31bde331236481b00d9b9c2ef740cd053
Sergio Pascual <sergiopr@fis.ucm.es>
2021-05-27 00:13:50 +0200

    Add mpl style

---
+++ rgblues/style.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 Universidad Complutense de Madrid
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+"""
+RGB predictions of Gaia EDR3 stars
+
+This code is hosted at https://github.com/nicocardiel/RGBfromGaiaEDR3
+Maintainer: Nicolás Cardiel <cardiel@ucm.es>
+
+matplotlib style
+"""
+
+mpl_style = {
+    'font.size': 17.0,
+    'font.family': 'serif',
+    'axes.linewidth': 2
+}
